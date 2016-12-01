9f0a9858430507f2ffd28344f4f68cd718a0d715
Everton <evertonvieira1994@gmail.com>
2016-11-30 23:29:59 -0200

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+var express        = require('express'),
+  path             = require('path'),
+  favicon          = require('static-favicon'),
+  logger           = require('morgan'),
+  cookieParser     = require('cookie-parser'),
+  bodyParser       = require('body-parser'),
+  load             = require('express-load');
+
+var app = express();
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+load('models').then('controllers').then('routes').into(app);
+
+app.listen(3000, function() {
+	console.log('Servidro rodando na porta 3000');
+});
