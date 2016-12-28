931c7f8f9dc0d3949ab37880d8af6bc36515f2b0
Everton <evertonvieira1994@gmail.com>
2016-12-28 18:38:05 -0200

    Adicionando thema do boostrap

--- controllers/home.js
+++ controllers/home.js
@@ -2,8 +2,10 @@ module.exports = function(app){
 
 	var HomeController = {
 		index: function(req, res){
-			res.render('home/index');
+			var titulo = "Home Page";
+			res.render('home/index',{titulo: titulo});
 		}
+
 	}
 
 	return HomeController;
