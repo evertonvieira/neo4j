ec752f6bef232bebeb750bca2a7654d7375d61ee
Everton Vieira <evertonvieira1994@gmail.com>
2018-12-02 21:14:03 -0200

    Adicionando edição de atores

--- controllers/atores.js
+++ controllers/atores.js
@@ -9,11 +9,65 @@ module.exports = function(app){
             process.env.NEO4J_PASSWORD
         )
     );
+    var titulo = "Atores"
 	const session = driver.session();
 
 	var AtoresController = {
 		index: function(req, res){	
 			
+        },
+
+        edit: function(req, res){
+
+			let paramId = req.params.id;           
+            
+            const cypher =  `
+            MATCH (v:Atores) WHERE id(v) = ${paramId} 
+            RETURN v
+            `
+            var ator = {};
+            			
+			session
+            .run(cypher)
+            .then(function(array){
+                array.records.forEach(function(record){
+                    ator  = {
+                        id: record._fields[0].identity.low,
+                        nome: record._fields[0].properties.nome,
+                        idade: record._fields[0].properties.idade,
+                    }
+                });						
+                res.render('atores/editar',{
+                    titulo: titulo,
+                    ator: ator,
+                });
+                
+            })
+            .catch(function(err){
+                console.log(err);
+            });
+        },
+
+        save: function (req, res){
+            let ator = req.body.ator;
+
+            const cypher =  `
+                MATCH (v:Atores) WHERE id(v) = ${ator.id}
+                SET v.nome = "${ator.nome}", v.idade = "${ator.idade}"
+                RETURN v
+                ` 
+            session.run(cypher)
+            .then(function(result){
+                req.flash('success', "Ator editado com sucesso!");
+                res.redirect("/")
+                session.close()
+            })
+            .catch(function(err){
+                req.flash('error', "Erro ao editar o Ator!");
+                res.redirect("/")
+                console.log(err)
+                session.close()
+            })
         },
 
         add: function(req, res){   
--- routes/atores.js
+++ routes/atores.js
@@ -3,5 +3,7 @@ module.exports = function(app){
     var atores = app.controllers.atores; 
     app.route('/atores').get(atores.index)
     app.route('/atores/add').post(atores.add);
+    app.route('/atores/editar/:id').get(atores.edit);
+    app.route('/atores/save').post(atores.save);
 
 }
